5ef6dd1ce1fbdfa96f0a611040d468038c19b804
aclark5 <alexclark36@gmail.com>
2022-06-30 21:42:43 -0400

    new csv and increase artists query for over 50

--- scripts/pull_artist.py
+++ scripts/pull_artist.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 def pull_artist_info(spotify_conn, artist_track):
     artist_ids = artist_track['artistID'].unique().tolist()
-    artist = spotify_conn.artists(artist_ids)
-    norm_artist = pd.json_normalize(artist['artists'])
+    if len(artist_ids) > 50:
+        artist_a = spotify_conn.artists(artist_ids[0:49])
+        norm_artist_a = pd.json_normalize(artist_a['artists'])
+        artist_b = spotify_conn.artists(artist_ids[49:])
+        norm_artist_b = pd.json_normalize(artist_b['artists'])
+        norm_artist = pd.concat([norm_artist_a, norm_artist_b])
+
+    else:
+        artist = spotify_conn.artists(artist_ids)
+        norm_artist = pd.json_normalize(artist['artists'])
+
     info_artist = norm_artist[['genres', 'id', 'popularity', 'followers.total']] \
         .rename(columns={'id': 'artistID', 'popularity': 'artistPopularity', 'followers.total': 'followers'})
     return info_artist
+
